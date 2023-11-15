89289c182a54d364d702c05a66cc17c686bcd392
River-souza <43649744+River-souza@users.noreply.github.com>
2023-11-15 15:31:30 -0300

    Implementação das rotas no index.js
    Foi implementado os códigos para as chamadas dos 'Endpoints' do sistema.

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ app.all("*", (req, res, next) => {
     next();
 
 });
+
+const routesUser = require('./src/routes/user-routes');
+
+const routesCategory = require('./src/routes/category-routes');
+
+const routesTransaction = require('./src/routes/transaction-routes');
+
+app.use(routesUser.routes);
+app.use(routesCategory.routes);
+app.use(routesTransaction.routes);
