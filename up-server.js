ddb46080e89a06a332e10bc61316e02f2e82acee
River-souza <43649744+River-souza@users.noreply.github.com>
2023-11-18 19:38:37 -0300

    Integração do front-end com a Api

--- up-server.js
+++ up-server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());//middleware nativo do express que só aceita informações no formato json
+
+app.use(cors('http://localhost:5173/'));//habilita acesso do frontend
+//app.use(cors);
 
 app.listen(process.env.PORT, () => {
 
