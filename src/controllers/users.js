12975bf107f179cb01f2abf2cf859eebdeb68e4c
River-souza <43649744+River-souza@users.noreply.github.com>
2023-12-05 11:26:27 -0300

    Correção de códigos

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,167 +1,86 @@
-const pool = require('../../storage/connection');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const pool = require("../../storage/connection");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function cadastrarUsuario(req, res) {
+  const { nome, email, senha } = req.body;
 
-    const { nome, email, senha } = req.body;
+  try {
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
 
-    try {
-        const passEncrypted = await bcrypt.hash(senha, 10);
+    const result = await pool.query(
+      "insert into usuarios (nome, email, senha) values ($1, $2, $3) returning id, nome, email",
+      [nome, email, senhaCriptografada]
+    );
 
-        //console.log(passEncrypted);
-
-        const { rows } = await pool.query(
-            'insert into usuarios (nome, email, senha) values ($1, $2, $3) returning id, nome, email', 
-            [nome, email, passEncrypted]
-            );
-
-        return res.status(201).json(rows[0]);
-
-    } catch (error) {
-
-        //console.log (error.message);
-        return res.status(500).json({ mensagem: "Erro interno do servidor!"});
-
-    }
+    return res.status(201).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor!" });
+  }
 }
 
 async function loginUsuario(req, res) {
+  const { email, senha } = req.body;
 
-    const { email, senha } = req.body;
+  try {
+    const result = await pool.query("select * from usuarios where email = $1", [
+      email,
+    ]);
 
-    try {
-
-        const user = await pool.query('select * from usuarios where email = $1', [email]);
-            
-        if (user.rowCount === 0) {
-
-            return res.status(400).json({ mensagem: 'Usuário e/ou senha invalido(s)!'});
-
-        }
-
-        const verifyPass = await bcrypt.compare(senha, user.rows[0].senha);
-
-        if (!verifyPass) {
-
-            return res.status(400).json({ mensagem: 'Usuário e/ou senha invalido(s)!'});
-
-        }
-
-        //return res.status(200).json({ mensagem: "Usuário autenticado."});
-        
-        const token = jwt.sign(
-            {id: user.rows[0].id }, 
-            process.env.JWT_PASSWORD, 
-            { expiresIn: '1d' }
-            );
-
-        const { senha: s, ...userLogado } = user.rows[0];
-
-        return res.status(200).json({ usuario: userLogado, token });
-
-    } catch (error) {
-
-        //console.log (error.message);
-        return res.status(500).json({ mensagem: "Erro interno do servidor!"});
-
-    }
-}
-
-/*async function logoutUsuario(req, res) {
-
-    req.usuario = {
-        id: 0,
-        nome: "",
-        emai: ""
+    if (result.rowCount === 0) {
+      return res
+        .status(400)
+        .json({ mensagem: "Usuário e/ou senha invalido(s)!" });
     }
 
-    req.headers.authorization = "";
-}*/
+    const senhaValidada = await bcrypt.compare(senha, result.rows[0].senha);
 
-async function listarUsuarios(req, res) {
-
-    try {
-        const result = await pool.query('select nome, email from usuarios');
-        
-        //console.log(result.rows);
-        return res.status(200).json(result.rows);
-        
-    } catch (error) {
-
-        //console.log (error.message);
-        return res.status(500).json({ mensagem: "Erro interno do servidor!"});
-
+    if (!senhaValidada) {
+      return res
+        .status(400)
+        .json({ mensagem: "Usuário e/ou senha invalido(s)!" });
     }
+
+    const token = jwt.sign(
+      { id: result.rows[0].id },
+      process.env.JWT_PASSWORD,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    const { senha: _, ...userLogado } = result.rows[0];
+
+    return res.status(200).json({ usuario: userLogado, token });
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor!" });
+  }
 }
 
 async function detalharUsuario(req, res) {
-
-    //console.log (req.usuario);
-    return res.status(200).json(req.usuario);
-
+  return res.status(200).json(req.usuario);
 }
 
 async function atualizarUsuario(req, res) {
+  const { nome, email, senha } = req.body;
 
-    const { nome, email, senha } = req.body;
+  try {
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
 
-    try {
+    await pool.query(
+      `update usuarios set nome = $1, email = $2, senha =$3 where id = $4`,
+      [nome, email, senhaCriptografada, req.usuario.id]
+    );
 
-        const passEncrypted = await bcrypt.hash(senha, 10);
-
-        await pool.query(
-            `update usuarios set nome = $1, email = $2, senha =$3 where id = $4`, 
-            [nome, email, passEncrypted, req.usuario.id]
-            );
-
-        return res.status(204).send();
-
-    } catch (error) {
-
-        //console.log (error.message);
-        return res.status(500).json({ mensagem: "Erro interno do servidor!"});
-
-    }
-}
-
-async function excluirUsuario (req, res) {
-    
-    const { id } = req.params;
-
-    if(isNaN(Number(id))) {
-
-        return res.status(400).json({ mensagem: 'O id passado é inválido!' });
-
-    }
-
-    if(Number(id) === 0) {
-
-        return res.status(400).json({ mensagem: 'O id passado é inválido!' });
-
-    }
-
-    try {
-        
-        await pool.query('delete from usuarios where id = $1', [id]);
-        
-        return res.status(204).send();
-        
-    } catch (error) {
-
-        //console.log (error.message);
-        return res.status(500).json({ mensagem: "Erro interno do servidor!"});
-
-    }
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor!" });
+  }
 }
 
 module.exports = {
-
-    cadastrarUsuario,
-    loginUsuario,
-    detalharUsuario,
-    atualizarUsuario,
-    listarUsuarios,//rotas para recursos do Adm
-    excluirUsuario//rotas para recursos do Adm
-
-}
+  cadastrarUsuario,
+  loginUsuario,
+  detalharUsuario,
+  atualizarUsuario,
+};
--- up-server.js
+++ up-server.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());//middleware nativo do express que só aceita informações no formato json
+app.use(express.json());
 
-app.use(cors('http://localhost:5173/'));//habilita acesso do frontend
-//app.use(cors);
+app.use(cors("http://localhost:5173/"));
 
 app.listen(process.env.PORT, () => {
-
-    console.log(`Servidor rodando na porta ${process.env.PORT}`);
-    
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
 });
 
 module.exports = app;
